65ed9f93c9c54aa7a9c0a39952b93502563ec664
Wang Shai <biz@jealer.com>
2026-08-03 04:51:07 +0800

    style

--- src/config/externalStyle.ts
+++ src/config/externalStyle.ts
@@ -1,30 +1,33 @@
-import accordion from 'vanilla-jui/src/css/accordion.css';
-import animation from 'vanilla-jui/src/css/animation.css';
-import button from 'vanilla-jui/src/css/button.css';
-import content from 'vanilla-jui/src/css/content.css';
-import drop from 'vanilla-jui/src/css/drop.css';
-import font from 'vanilla-jui/src/css/font.css';
-import form from 'vanilla-jui/src/css/form.css';
-import icon from 'vanilla-jui/src/css/icon.css';
-import layout from 'vanilla-jui/src/css/layout.css';
-import menu from 'vanilla-jui/src/css/menu.css';
-import modal from 'vanilla-jui/src/css/modal.css';
-import offcanvas from 'vanilla-jui/src/css/offcanvas.css';
-import popup from 'vanilla-jui/src/css/popup.css';
-import status from 'vanilla-jui/src/css/status.css';
-import tabs from 'vanilla-jui/src/css/tabs.css';
-import themePalette from 'vanilla-jui/src/css/theme-palette.css';
-import accordionTheme from 'vanilla-jui/src/css/themes/default/accordion.css';
-import buttonTheme from 'vanilla-jui/src/css/themes/default/button.css';
-import dropTheme from 'vanilla-jui/src/css/themes/default/drop.css';
-import modalTheme from 'vanilla-jui/src/css/themes/default/modal.css';
-import offcanvasTheme from 'vanilla-jui/src/css/themes/default/offcanvas.css';
-import popupTheme from 'vanilla-jui/src/css/themes/default/popup.css';
-import tabsTheme from 'vanilla-jui/src/css/themes/default/tabs.css';
-import tipTheme from 'vanilla-jui/src/css/themes/default/tip.css';
-import tip from 'vanilla-jui/src/css/tip.css';
-import toc from 'vanilla-jui/src/css/toc.css';
-import tokens from 'vanilla-jui/src/css/tokens.css';
+import accordion from "vanilla-jui/src/css/accordion.css";
+import animation from "vanilla-jui/src/css/animation.css";
+import button from "vanilla-jui/src/css/button.css";
+import content from "vanilla-jui/src/css/content.css";
+import contentTheme from "vanilla-jui/src/css/themes/default/content.css";
+import drop from "vanilla-jui/src/css/drop.css";
+import font from "vanilla-jui/src/css/font.css";
+import form from "vanilla-jui/src/css/form.css";
+import icon from "vanilla-jui/src/css/icon.css";
+import layout from "vanilla-jui/src/css/layout.css";
+import menu from "vanilla-jui/src/css/menu.css";
+import menuTheme from "vanilla-jui/src/css/themes/default/menu.css";
+import modal from "vanilla-jui/src/css/modal.css";
+import offcanvas from "vanilla-jui/src/css/offcanvas.css";
+import popup from "vanilla-jui/src/css/popup.css";
+import status from "vanilla-jui/src/css/status.css";
+import tabs from "vanilla-jui/src/css/tabs.css";
+import themePalette from "vanilla-jui/src/css/theme-palette.css";
+import themePaletteTheme from "vanilla-jui/src/css/themes/default/theme-palette.css";
+import accordionTheme from "vanilla-jui/src/css/themes/default/accordion.css";
+import buttonTheme from "vanilla-jui/src/css/themes/default/button.css";
+import dropTheme from "vanilla-jui/src/css/themes/default/drop.css";
+import modalTheme from "vanilla-jui/src/css/themes/default/modal.css";
+import offcanvasTheme from "vanilla-jui/src/css/themes/default/offcanvas.css";
+import popupTheme from "vanilla-jui/src/css/themes/default/popup.css";
+import tabsTheme from "vanilla-jui/src/css/themes/default/tabs.css";
+import tipTheme from "vanilla-jui/src/css/themes/default/tip.css";
+import tip from "vanilla-jui/src/css/tip.css";
+import toc from "vanilla-jui/src/css/toc.css";
+import tokens from "vanilla-jui/src/css/tokens.css";
 
 export default [
   tokens,
@@ -33,11 +36,13 @@ export default [
   font,
   form,
   content,
+  contentTheme,
   animation,
   status,
   button,
   buttonTheme,
   menu,
+  menuTheme,
   popup,
   popupTheme,
   modal,
@@ -54,4 +59,5 @@ export default [
   accordionTheme,
   toc,
   themePalette,
+  themePaletteTheme,
 ];
